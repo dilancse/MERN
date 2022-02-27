a1ae8b6352edd3676f3625ff7ad17a1867c5df54
g_dilan <g.dilan@directfn.com>
2022-02-27 17:47:50 +0530

    'add-express-server'

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const express = require("express");
+const dotEnv = require("dotenv").config();
+const port = process.env.PORT;
+
+const app = express();
+
+app.get('/api/goals', (req, res, next)=>{
+    res.status(200).json({message: 'Get goals'});
+})
+
+app.listen(port, () => {
+  console.log(`server started on port ${port}`);
+});
